815d46deeb77ea0717f59fccec401a1d8369c54e
Ayush Mishra <98708590+theayushmishra@users.noreply.github.com>
2024-08-21 17:50:18 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "34jh"
+
+let scoreInNumber = Number(score);      // type conversion
+// console.log(typeof score);              // string
+// console.log(typeof scoreInNumber);      // number
+// console.log(scoreInNumber);             // NaN // i.e. Not a Number
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
