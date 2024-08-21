3d0abb3f726b8dc76a855baa08ef833e07d9ee5f
Ayush Mishra <98708590+theayushmishra@users.noreply.github.com>
2024-08-21 17:50:46 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14553
+let accountEmail = "ayush.mishra@gmail.com"
+var accountPassword = "12345"
+accountCity = "Raigarh"
+
+// accountId = 1554 // not allowed
+
+accountEmail = "ayush.mishra@yahoo.com"
+accountPassword = "9876"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict" // use newer version of JS
+
+// alert( 2 + 3 ) // we are using NodeJS, not browser 
+
+let name = "Ayush"
+
+let age = 28
+
+let isLoggedIn = true
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);       // object
+
