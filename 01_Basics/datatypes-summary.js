95869483b690e5f6009296d6c42f17d799ad54ba
Ayush Mishra <98708590+theayushmishra@users.noreply.github.com>
2024-08-22 16:47:52 +0000

    datatypes summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+// primitive and non-primitive (or reference types) data types
+
+let userName = "Ayush" // typeof - string
+let num = 100 // typeof - number
+let flag1 = true // typeof - boolean
+// typeof - bigInt
+let temperature = null  // typeof - object ****
+let value;  // typeof - undefined
+    // symbol data type
+
+// console.log(typeof bigNum);
+
+// non-primitive or reference types
+//array
+let arr1 = ["Ayush", 28, "Raigarh"] //typeof - object
+
+//function
+let myFunction = function(){
+    console.log("this is a function");
+    
+}
+
+//object
+let myObj = {
+    name : "Ayush",
+    age : 22
+};
+
+// console.log(typeof arr1);   // object
+// console.log(typeof myFunction); // function ( function object)
+console.log(typeof myObj);  // object
+
