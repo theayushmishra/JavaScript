2ab0192157126cd35d89ac4d7b3eb2bfb371530f
Ayush Mishra <98708590+theayushmishra@users.noreply.github.com>
2024-08-22 15:40:45 +0000

    comparison operations

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,11 @@
+// console.log(2 > 1);
+// console.log(null == 0);
+
+// console.log("0" === "0");   // strict check, it will check the data types as well as the values
+
+// console.log(undefined == 0);
+
+// console.log(null == 0); // equality check does not convert null into 0
+
+console.log(null >= 0); // while comparison operations change the null into 0, so the result will be true
+// or console.log(null <= 0);
